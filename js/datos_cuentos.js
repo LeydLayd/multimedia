26d0090dc7956a1bfa18ce6041053c78b3e0a119
LaydLayd <rojasdiego035@gmail.com>
2025-12-02 10:26:05 -0600

    Uploading

---
+++ js/datos_cuentos.js
@@ -0,0 +1,94 @@
+const cuentosData = {
+    // --- CLÁSICOS (Biblioteca) ---
+    "cerditos": {
+        titulo: "Los Tres Cerditos",
+        texto: "Había una vez tres cerditos que decidieron irse a vivir al bosque.<br><br>El primero, que era un poco perezoso, hizo su casa de paja en un santiamén y se echó a dormir. El segundo, que trabajaba un poco más, hizo su casa de madera. Pero el tercero, que era muy trabajador, tardó varios días en construir una casa fuerte de ladrillo.<br><br>Un día apareció el lobo feroz y sopló y sopló... ¡y la casa de paja derribó! El cerdito corrió a la casa de madera. El lobo sopló de nuevo y la casa de madera también cayó. Los dos cerditos corrieron a la casa de ladrillo.<br><br>El lobo sopló con todas sus fuerzas hasta ponerse morado, pero la casa de ladrillo no se movió ni un milímetro. Cansado y hambriento, el lobo se fue y los tres cerditos vivieron felices y seguros para siempre.",
+        icono: "🐷",
+        colorFondo: "bg-pink"
+    },
+    "caperucita": {
+        titulo: "Caperucita Roja",
+        texto: "Había una niña muy dulce que siempre llevaba una capa roja con capucha, por eso todos la llamaban Caperucita Roja.<br><br>Un día, su mamá le pidió que llevara una cesta con pasteles y miel a su abuelita, que vivía al otro lado del bosque. 'No te entretengas ni hables con extraños', le advirtió su mamá.<br><br>En el camino, Caperucita se encontró con el lobo, quien la retó a una carrera hasta la casa de la abuela. El lobo tomó el camino corto y Caperucita el largo. Cuando Caperucita llegó, notó que su 'abuelita' tenía orejas muy grandes y dientes muy grandes... ¡Era el lobo disfrazado!<br><br>Por suerte, un cazador que pasaba por ahí escuchó los gritos, espantó al lobo y salvó a Caperucita y a su abuelita. Desde ese día, Caperucita prometió siempre hacer caso a su mamá.",
+        icono: "🐺",
+        colorFondo: "bg-sunset"
+    },
+    "pinocho": {
+        titulo: "Pinocho",
+        texto: "Geppetto era un bondadoso carpintero que se sentía muy solo. Un día, talló una marioneta de madera y la llamó Pinocho. 'Ojalá fuera un niño de verdad', suspiró.<br><br>Esa noche, el Hada Azul concedió su deseo y le dio vida a la madera, pero le advirtió: 'Para ser un niño de verdad, debes ser bueno, sincero y generoso'. También le dio a Pepe Grillo como su conciencia.<br><br>Pinocho vivió muchas aventuras y cometió errores; cada vez que mentía, ¡su nariz crecía y crecía! Pero al final, demostró ser valiente salvando a su papá Geppetto de una ballena gigante. El Hada Azul, al ver su valentía, lo convirtió en un niño de carne y hueso, y vivieron felices.",
+        icono: "🤥",
+        colorFondo: "bg-cyan"
+    },
+    "patito_feo": {
+        titulo: "El Patito Feo",
+        texto: "En una granja nació un patito que era diferente a los demás. Era más grande, más torpe y de color gris. Todos los animales se burlaban de él y le llamaban 'El Patito Feo'.<br><br>El pobre patito se sentía muy triste y decidió irse lejos. Pasó el invierno solo y con frío, escondiéndose para que nadie lo molestara.<br><br>Cuando llegó la primavera, se acercó a un estanque donde nadaban unos hermosos cisnes blancos. 'Seguro se burlarán de mí', pensó. Pero al mirar su reflejo en el agua, se llevó una sorpresa: ¡Ya no era un patito gris y torpe! Se había convertido en un precioso cisne blanco. Ahora todos lo admiraban, y él comprendió que ser diferente no es malo, solo estaba creciendo.",
+        icono: "🦆",
+        colorFondo: "bg-green"
+    },
+    "tortuga_liebre": {
+        titulo: "La Liebre y la Tortuga",
+        texto: "La liebre siempre se burlaba de la tortuga por ser tan lenta. '¡Te apuesto a que te gano una carrera!', dijo la liebre presumiendo.<br><br>La carrera comenzó y la liebre salió disparada, dejando a la tortuga muy atrás en una nube de polvo. Confiada en que ganaría fácil, la liebre decidió echarse una siesta a la mitad del camino bajo un árbol.<br><br>La tortuga, sin prisa pero sin pausa, siguió caminando pasito a pasito. Cuando la liebre despertó, vio con horror que la tortuga estaba a punto de cruzar la meta. Corrió lo más rápido que pudo, pero fue tarde. La tortuga ganó la carrera, demostrando que la constancia vale más que la velocidad.",
+        icono: "🐢",
+        colorFondo: "bg-math"
+    },
+
+    "ricitos": {
+        titulo: "Ricitos de Oro",
+        texto: "Una niña rubia y curiosa llamada Ricitos de Oro encontró una casita en el bosque y entró sin permiso. Vio tres platos de sopa: uno muy caliente, uno muy frío y uno perfecto, ¡y se lo comió todo!<br><br>Luego vio tres sillas: una muy grande, una mediana y una pequeñita que... ¡crac! rompió al sentarse. Finalmente, encontró tres camas y se quedó dormida en la más pequeña.<br><br>De pronto, llegaron los dueños: Papá Oso, Mamá Osa y Bebé Oso. Al ver a la niña, ella se despertó del susto y salió corriendo por la ventana, aprendiendo que no se debe entrar en casa ajena sin invitación.",
+        icono: "👧",
+        colorFondo: "bg-sunset"
+    },
+    "hansel_gretel": {
+        titulo: "Hansel y Gretel",
+        texto: "Hansel y Gretel eran dos hermanos que se perdieron en el bosque. Intentaron dejar migas de pan para volver, pero los pajaritos se las comieron.<br><br>Caminando, encontraron una casa mágica hecha de chocolate, galletas y dulces. ¡Qué rico! Empezaron a comer, pero la casa era de una bruja que quería atraparlos.<br><br>Trabajando en equipo y siendo muy valientes, los hermanos lograron engañar a la bruja y escapar, encontrando el camino de vuelta a casa con su papá.",
+        icono: "🍭",
+        colorFondo: "bg-menu"
+    },
+    "leon_raton": {
+        titulo: "El León y el Ratón",
+        texto: "Un poderoso león dormía la siesta cuando un pequeño ratón empezó a jugar en su melena. El león lo atrapó enfadado, pero el ratón le suplicó: 'Por favor, déjame ir, algún día podré ayudarte'. El león se rió pensando que un animal tan pequeño nunca podría ayudarlo, pero lo soltó.<br><br>Días después, el león quedó atrapado en una red de cazadores. Rugió pidiendo ayuda, y el ratoncito apareció. Con sus dientes afilados, royó la cuerda hasta romperla y liberar al león.<br><br>Desde ese día, fueron grandes amigos, y el león aprendió que los pequeños también pueden hacer grandes cosas.",
+        icono: "🦁",
+        colorFondo: "bg-orange"
+    },
+    "cenicienta": {
+        titulo: "La Cenicienta",
+        texto: "Cenicienta era una joven muy bondadosa a la que sus hermanastras obligaban a limpiar todo el día. Cuando el Príncipe organizó un baile, ella no podía ir porque no tenía vestido.<br><br>Pero apareció su Hada Madrina y, con un '¡Bibidi Babidi Bu!', convirtió una calabaza en carroza y sus harapos en un vestido azul precioso, advirtiéndole que el hechizo terminaría a medianoche.<br><br>Cenicienta bailó con el Príncipe, pero al dar las doce salió corriendo y perdió un zapato de cristal. El Príncipe buscó a la dueña del zapato por todo el reino hasta encontrarla, y vivieron felices para siempre.",
+        icono: "👠",
+        colorFondo: "bg-cyan"
+    },
+    "blancanieves": {
+        titulo: "Blancanieves",
+        texto: "Había una princesa de piel blanca como la nieve que tuvo que huir al bosque porque su madrastra tenía envidia de su belleza. Allí encontró una casita pequeña donde vivían siete enanitos trabajadores.<br><br>Blancanieves se quedó a vivir con ellos, cantando y cuidando la casa. Pero la madrastra, disfrazada de anciana, le dio una manzana hechizada que la hizo dormir profundamente.<br><br>Solo el beso de amor verdadero podría despertarla. Un príncipe que pasaba por allí la vio, la besó, y Blancanieves despertó feliz, despidiéndose de los enanitos para vivir nuevas aventuras.",
+        icono: "🍎",
+        colorFondo: "bg-pink"
+    },
+    "cigarra_hormiga": {
+        titulo: "La Cigarra y la Hormiga",
+        texto: "Durante un verano caluroso, la cigarra se pasaba el día cantando y descansando bajo la sombra, mientras la hormiga trabajaba sin parar llevando comida a su hormiguero.<br><br>'¿Por qué trabajas tanto? ¡Ven a cantar!', decía la cigarra. 'Estoy guardando comida para el invierno', respondió la hormiga.<br><br>Cuando llegó el frío invierno, la cigarra tenía hambre y frío, mientras que la hormiga estaba calentita y con mucha comida. La hormiga, que era buena, compartió con ella, y la cigarra aprendió que hay tiempo para divertirse y tiempo para trabajar.",
+        icono: "🐜",
+        colorFondo: "bg-green"
+    },
+    "gato_botas": {
+        titulo: "El Gato con Botas",
+        texto: "Un molinero le dejó a su hijo menor solo un gato como herencia. El chico estaba triste, pero el gato le dijo: 'Dame unas botas y un sombrero, y verás de lo que soy capaz'.<br><br>El gato, muy astuto, engañó al Rey y a un ogro malvado para conseguir riquezas y un castillo para su amo, haciéndolo pasar por el 'Marqués de Carabás'.<br><br>Gracias a la inteligencia de su gato, el joven pobre terminó casándose con la princesa y viviendo muy feliz.",
+        icono: "👢",
+        colorFondo: "bg-math"
+    },
+    "peter_pan": {
+        titulo: "Peter Pan",
+        texto: "Peter Pan es el niño que nunca crece y vive en el País de Nunca Jamás junto al hada Campanita. Una noche, visitó a Wendy y a sus hermanos y, rociándolos con polvo de hadas, salieron volando por la ventana.<br><br>Vivieron aventuras increíbles luchando contra el Capitán Garfio y los piratas. Garfio le tenía mucho miedo a un cocodrilo que hacía 'tic-tac'.<br><br>Aunque Peter quería que se quedaran siempre, Wendy sabía que tenía que crecer, así que regresaron a casa guardando el recuerdo de la magia en sus corazones.",
+        icono: "🧚",
+        colorFondo: "bg-cyan"
+    },
+    "lechera": {
+        titulo: "La Lechera",
+        texto: "Una niña iba al mercado llevando un cántaro de leche en la cabeza. Por el camino, iba imaginando: 'Venderé la leche y compraré huevos. Los huevos me darán pollitos. Cuando crezcan, los venderé y compraré un cerdito... y luego una vaca...'.<br><br>Estaba tan distraída soñando con todo el dinero que ganaría, que tropezó con una piedra. ¡Plaf!<br><br>El cántaro se rompió y la leche se derramó. Adiós huevos, adiós pollitos y adiós vaca. La niña aprendió que no hay que contar con lo que aún no tenemos, y que es mejor vivir el presente.",
+        icono: "🥛",
+        colorFondo: "bg-pink"
+    },
+    "juan_habichuelas": {
+        titulo: "Juan y los Frijoles",
+        texto: "Juan cambió su vaca por unos frijoles mágicos. Su mamá se enfadó y los tiró por la ventana, pero a la mañana siguiente, ¡había crecido una planta gigante que llegaba hasta el cielo!<br><br>Juan trepó y encontró el castillo de un gigante. Allí vio una gallina que ponía huevos de oro. El gigante lo descubrió y gritó: '¡Fi-Fa-Fo-Fum!', pero Juan logró escapar bajando rápido por la planta.<br><br>Cortó el tallo y el gigante ya no pudo bajar. Con la gallina de los huevos de oro, Juan y su mamá nunca más pasaron hambre.",
+        icono: "🌱",
+        colorFondo: "bg-green"
+    }
+};
